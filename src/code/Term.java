cc29998c815f2efdfb9e34d79df375deb6fde3b5
DavidKJones <emaildkj@gmail.com>
2018-02-23 16:25:14 -0700

    Add files via upload

---
+++ src/code/Term.java
@@ -0,0 +1,15 @@
+package code;
+/**
+ * CSIS 2420
+ * A03_AutoComplete Assignment
+ * @author David Jones
+ */
+public class Term implements Comparable<Term> 
+{
+	@Override
+	public int compareTo(Term t) 
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
